af97bf5758fe95781c6169819de48e7461200be9
amanjiw <ghaderi.amanj@gmail.com>
2024-03-21 00:03:59 +0330

    avatar component added to userlist

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,8 +1,14 @@
+import useUsers from "@/hooks/useUsers";
 import React from "react";
+import Avatar from "../Avatar";
 
 interface FollowBarProps {}
 
 const FollowBar = () => {
+	const { data: users = [] } = useUsers();
+
+	if (users.length === 0) return null;
+
 	return (
 		<div className="px-6 py-4 hidden lg:block">
 			<div className="bg-neutral-800 rounded-xl p-4">
@@ -11,6 +17,22 @@ const FollowBar = () => {
 				</h2>
 				<div className="flex flex-col gap-6 mt-4">
 					{/* todo user list */}
+
+					{users.map((user: Record<string, any>) => {
+						return (
+							<div className="flex flex-row gap-4" key={user.id}>
+								<Avatar userId={user.id} />
+								<div className="flex flex-col">
+									<p className="text-white font-semibold text-sm">
+										{user.name}
+									</p>
+									<p className="text-neutral-400 text-sm">
+										@{user.username}
+									</p>
+								</div>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</div>
--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -14,3 +14,6 @@ const useUser = (userId: string) => {
 		mutate,
 	};
 };
+
+
+export default useUser
