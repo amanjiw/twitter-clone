e0ae697a028e0519f2d57de58a61a2bc9aa86097
amanjiw <ghaderi.amanj@gmail.com>
2024-03-26 11:10:53 +0330

    click on like fixed

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -73,7 +73,7 @@ const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
 						</div>
 						<div
 							className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
-							onCanPlay={onLike}
+							onClick={onLike}
 						>
 							<AiOutlineHeart size={20} />
 							<p>{data?.likes?.length || 0}</p>
